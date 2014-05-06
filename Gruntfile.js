68bc7a60fab80c1e117fc34e2d3bce406b39ecd8
rodrigoibarra <hola@rodrigo-ibarra.com>
2014-05-06 14:44:46 -0500

    pretty much did the whole basic site in one run, still need to go back and fix some shit but its good...

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: ['dev/js/*.js', 'js/global.js'],
+        dest: 'build/js/global.min.js'
+      }
+    },
+      compass: {                  // Task
+        dist: {                   // Target
+          options: {              // Target options
+            sassDir: 'dev/scss/',
+            cssDir: 'build/css/',
+            environment: 'production'
+          }
+        }
+      },
+
+      watch: {
+          css: {
+            files: '**/*.scss',
+            tasks: ['compass'],
+            options: {
+              livereload: true,
+            },
+          },
+           scripts: {
+            files: 'js/*.js',
+            tasks: ['uglify'],
+            options: {
+              debounceDelay: 250,
+            },
+          },
+      },
+
+      connect: {
+        uses_defaults: {}
+      }
+
+  });
+
+  // Load the plugin that provides the tasks.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'compass']);
+
+};
