4362398a8b6cc8a95e40945b8a871883e37b1519
rodrigoibarra <hola@rodrigo-ibarra.com>
2014-05-13 18:07:09 -0500

    added fitvid to work with the talk videos... dont like how they look... they look i dont know

--- dev/js/global.js
+++ dev/js/global.js
@@ -1,3 +1,4 @@
-$(document).ready(function() {
-    $("#biggie h2").lettering('words'); 
+  $(document).ready(function(){
+    // Target your .container, .wrapper, .post, etc.
+    $(".media").fitVids();
   });
