a0b86f414348e405aee6b9ba01e2974aa5d352be
rodrigoibarra <hola@rodrigo-ibarra.com>
2014-05-15 19:55:57 -0500

    fixed the body class issue

--- functions.php
+++ functions.php
@@ -1,15 +1,3 @@
 <?php
  wp_enqueue_script("jquery");
-    add_filter('body_class','add_category_to_single');
-    function add_category_to_single($classes, $class) {
-      if (is_single() ) {
-        global $post;
-        foreach((get_the_category($post->ID)) as $category) {
-          // add category slug to the $classes array
-          $classes[] = $category->category_nicename;
-        }
-      }
-      // return the $classes array
-      return $classes;
-    }
 ?>
