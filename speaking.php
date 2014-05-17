5a36d120988d08ac4f351cc7d2c9965a5f4d43ca
rodrigoibarra <hola@rodrigo-ibarra.com>
2014-05-16 20:23:27 -0500

    fixed speaking template

--- speaking.php
+++ speaking.php
@@ -4,12 +4,12 @@
     */
 get_header(); ?>
   <section>
-    <div class="talks">
+    <article class="talks">
     <?php if (have_posts()) : while (have_posts()) : the_post();?>
 
       <?php the_content('<p class="serif">Read the rest of this page »</p>'); ?>
 
     <?php endwhile; endif; ?>
-    </div>
+    </article>
   </section>
 <?php get_footer(); ?>
