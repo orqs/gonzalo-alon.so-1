ab64f5ed1cf7205f955c6aad541e2ba46f894b4f
rodrigoibarra <hola@rodrigo-ibarra.com>
2014-05-28 15:24:29 -0500

    fixed the images size for all blogposts and added the analytics to functions.php

--- functions.php
+++ functions.php
@@ -5,5 +5,17 @@ function my_jquery_enqueue() {
    wp_deregister_script('jquery');
    wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", false, null);
    wp_enqueue_script('jquery');
-}
-?>
+}?>
+<?php
+add_action('wp_footer', 'add_googleanalytics');
+function add_googleanalytics() { ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-51263957-1', 'gonzalo-alonso.com');
+    ga('send', 'pageview');
+  </script>
+<?php } ?>
