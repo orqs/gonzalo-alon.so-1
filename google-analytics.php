8806721e3df0303470a8ce7c8e1677671e12c12b
orqs <olicona@gmail.com>
2014-08-04 14:11:38 -0500

    Create google-analytics.php

---
+++ google-analytics.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Google Analytics
+Description: Adds Google Analytics
+Author: me
+Version: 1.0
+*/
+
+add_action( 'wp_head', 'add_ga_script' );
+
+function add_ga_script()
+{
+?>
+<script type="text/javascript">
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-51263957-1', 'gonzalo-alonso.com');
+    ga('send', 'pageview');
+</script>
+<?php
+}
