4828a39a16ce49aa14631f86785864d47a5c0689
orqs <olicona@gmail.com>
2014-08-04 14:21:24 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -2,18 +2,18 @@
   <h1 class="logo">Gonzalo</br>Alonso</h1>
   <a href="#menu" class="menu-link">Menu</a>
   <nav id="menu" role="navigation">
-    <li><a class="home" href="/home/">home</a></li>
+    <li><a class="home" href="/home/">inicio</a></li>
     <li><a class="bio" href="/bio/">bio</a></li>
-    <li><a class="speaking" href="/speaking/">speaker</a></li>
-    <li><a href="/publish/" class="publish">published</a></li>
+    <li><a class="speaking" href="/speaking/">conferencista</a></li>
+    <li><a href="/publish/" class="publish">publicaciones</a></li>
     <li><a class="blog" href="/blog/">blog</a></li>
-    <li><a class="newsletter" href="/newsletter/">newsletter</a></li>
-    <li><a class="contact" href="/contact/">contact</a></li>
+    <li><a class="newsletter" href="/newsletter/">boletín</a></li>
+    <li><a class="contact" href="/contact/">contacto</a></li>
     <li>
-      <a class="social symbol" href="http://twitter.com/gonzoogle">&#xe286;</a>
-      <a class="social symbol" href="http://mx.linkedin.com/in/gonzoalonso" >&#xe252;</b></a>
-      <a class="social symbol" href="http://instagram.com/gonzoogle">&#xe300;</a>
-      <a class="social symbol" href="hhttps://www.facebook.com/pages/Gonzalo-Alonso/1454128954807284">&#xe227;</a>
+      <a class="social symbol" target="_blank" href="http://twitter.com/gonzoogle">&#xe286;</a>
+      <a class="social symbol" target="_blank" href="http://mx.linkedin.com/in/gonzoalonso" >&#xe252;</b></a>
+      <a class="social symbol" target="_blank" href="http://instagram.com/gonzoogle">&#xe300;</a>
+      <a class="social symbol" target="_blank" href="hhttps://www.facebook.com/pages/Gonzalo-Alonso/1454128954807284">&#xe227;</a>
     </li>
   </nav>
 </div>
