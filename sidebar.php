23c5f86008cd605ca0971ef634c4a51aee39f0b8
orqs <olicona@gmail.com>
2014-08-04 13:27:39 -0500

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,19 @@
     		<img src="<?php bloginfo('template_url'); ?>/build/images/rogue-squadron.png" alt="moño" />
 
         <?php get_search_form(); ?>
-      <ul>
-        <li><a href="http://gonzalo-alonso.com/feed/rss2/">RSS Feed</a></li>
-        <li><a class="social symbol" href="http://twitter.com/gonzoogle">Twitter &#xe286;</a></li>
-        <li><a class="social symbol" href="hhttps://www.facebook.com/pages/Gonzalo-Alonso/1454128954807284">FaceBook &#xe227;</a></li>
-        <li><a class="social symbol" href="http://instagram.com/gonzoogle">Instagram &#xe300;</a></li>
-        <li><a class="social symbol" href="http://mx.linkedin.com/in/gonzoalonso" >LinkedIn &#xe252;</a></li>
-      </ul>
 
+	<h3><?php _e( 'Publicaciones por categoría' ); ?></h3>
 
-				<h3><?php _e( 'Archives' ); ?></h3>
+		<?php wp_list_categories( 'orderby=name&title_li=' ); ?>
+
+	<h3><?php _e( 'Publicaciones por mes' ); ?></h3>
 
 					<?php wp_get_archives( 'type=monthly' ); ?>
+
+      <ul>
+        <li><a target="_blank" href="http://gonzalo-alonso.com/feed/rss2/">RSS Feed</a></li>
+        <li><a class="social symbol" target="_blank" href="http://twitter.com/gonzoogle">Twitter &#xe286;</a></li>
+        <li><a class="social symbol" target="_blank" href="hhttps://www.facebook.com/pages/Gonzalo-Alonso/1454128954807284">FaceBook &#xe227;</a></li>
+        <li><a class="social symbol" target="_blank" href="http://instagram.com/gonzoogle">Instagram &#xe300;</a></li>
+        <li><a class="social symbol" target="_blank" href="http://mx.linkedin.com/in/gonzoalonso" >LinkedIn &#xe252;</a></li>
+      </ul>
