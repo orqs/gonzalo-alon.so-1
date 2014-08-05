c533fd429129925ac705bf810e4b21d91f496862
orqs <olicona@gmail.com>
2014-08-05 02:22:07 -0500

    Update lpages.php

--- lpages.php
+++ lpages.php
@@ -1,11 +1,11 @@
 <?php
     /*
-    Template Name: lpages
+    Template Name: Lpages
     */
 get_header(); ?>
     <section class="lpages">
       <?php if (have_posts()) : while (have_posts()) : the_post();?>
-      <div class="lp-one-right">
+      <div class="one-left">
         <?php the_content('<p class="serif">Read the rest of this page �</p>'); ?>
 </div>
       <?php endwhile; endif; ?>
